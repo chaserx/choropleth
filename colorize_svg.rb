3fe82fe6d3067f6c28e60cd3b82b0b8899e2686f
Chase Southard <chase.southard@gmail.com>
2015-03-13 23:54:00 -0400

    better style

--- colorize_svg.rb
+++ colorize_svg.rb
@@ -1,28 +1,18 @@
 #!/usr/local/bin/ruby -wKU
 
-require "rubygems"
 require "nokogiri"
 require "fastercsv"
 
-# 12.12.09
-# Chase Southard
-# chase {dot} southard [at] gmail {dot} com
-
-# About
-# This is a rework of Nathan Yau's post on his blog Flowing Data entitled "How to Make a US County Thematic Map Using Free Tools."
-# http://flowingdata.com/2009/11/12/how-to-make-a-us-county-thematic-map-using-free-tools/
-
-
 # Step 1 - Pull the county specific data from a csv file and county code and unemployment rate into a hash
 unemployment = {}
-FasterCSV.foreach("unemployment09.csv") { |csv|
+FasterCSV.foreach(File.expand_patht('data/cleaned/unemployment14.csv')) do |csv|
   full_fips = csv[1]+csv[2]
   rate = csv[8].strip!.to_f
-  unemployment[full_fips] = rate 
-}
+  unemployment[full_fips] = rate
+end
 
 # Step 2 - Open the blank map with Nokogiri
-doc = Nokogiri::XML(File.open("USA_Counties_with_FIPS_and_names.svg"))
+doc = Nokogiri::XML(File.open(File.expand_path('svg/USA_Counties_with_FIPS_and_names.svg'))
 
 # pull paths into a nokogiri node set
 # this took hours to figure out that you need to have the xmlns part in place. doesn't appear to work without.
@@ -35,38 +25,37 @@ path_style = "font-size:12px;fill-rule:nonzero;stroke:#FFFFFF;stroke-opacity:1;
 stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-linecap:butt;
 marker-start:none;stroke-linejoin:bevel;fill:"
 
-for path in paths
-      
-      #a little debug output
-      puts path['id']
-      rate = unemployment[path['id']]
-      puts rate
-    
-      if rate == nil then rate = 0 end
-    
-      if rate > 10
-        path['style'] = path_style + colors[5]
-      elsif rate > 8
-        path['style'] = path_style + colors[4]
-      elsif rate > 6
-        path['style'] = path_style + colors[3]
-      elsif rate > 4
-        path['style'] = path_style + colors[2]
-      elsif rate > 2
-        path['style'] = path_style + colors[1]
-      #elsif rate == 0
-        #color_class = 0
-        #puts "I'm a 0"
-        #path['style'] = path_style + colors[0]
-      else
-        path['style'] = path['style']
-      end
-      
-      # debug: puts the style 
-      puts path['style']    
+paths.each do |path|
+  # a little debug output
+  puts path['id']
+  rate = unemployment[path['id']]
+  puts rate
+
+  if rate == nil then rate = 0 end
+
+  if rate > 10
+    path['style'] = path_style + colors[5]
+  elsif rate > 8
+    path['style'] = path_style + colors[4]
+  elsif rate > 6
+    path['style'] = path_style + colors[3]
+  elsif rate > 4
+    path['style'] = path_style + colors[2]
+  elsif rate > 2
+    path['style'] = path_style + colors[1]
+  # elsif rate == 0
+    # color_class = 0
+    # puts "I'm a 0"
+    # path['style'] = path_style + colors[0]
+  else
+    path['style'] = path['style']
+  end
+
+  # debug: puts the style
+  puts path['style']
 end
 
 # Step 4 - create a new file and push the modified svg structure into the new outfile
-outfile = File.new("outfile.svg", "w")
+outfile = File.new('unemployment14.svg', 'w')
 outfile << doc.to_s
 
